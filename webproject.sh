79e2353050da1ed3ed0e48a05b7198bc978b4034
rogerbf <roger@bergfalk.nu>
2016-10-16 16:31:43 +0200

    css transpilation

--- webproject.sh
+++ webproject.sh
@@ -72,15 +72,50 @@ webproject () {
       package.scripts['clean:production'] = 'rimraf build/production'
       package.scripts['clean:all'] = 'npm-run-all clean:development clean:production'
       package.scripts['babel'] = 'rimraf build/.babel && mkdirp build/.babel && babel --out-dir build/.babel source'
+      package.scripts['build:development'] = 'npm-run-all --parallel build:development:*'
+      package.scripts['build:production'] = 'npm-run-all --parallel build:production:*'
       package.scripts['build:development:js'] = 'npm run babel && rollup --sourcemap inline --output build/development/bundle.js build/.babel/index.js'
       package.scripts['build:production:js'] = 'npm run babel && mkdirp build/production && rollup build/.babel/index.js | uglifyjs --mangle --compress --output build/production/bundle.js && rimraf build/.babel-output'
       package.scripts['build:development:html'] = 'html-minifier --file-ext html --input-dir source --output-dir build/development'
       package.scripts['build:production:html'] = 'html-minifier --collapse-whitespace --remove-comments --minify-css --file-ext html --input-dir source --output-dir build/production'
-      package.scripts['build:development'] = 'npm-run-all --parallel build:development:js build:development:html'
       package.scripts['server:development'] = 'browser-sync start --server build/development --files build/development --no-open'
       fs.writeFileSync('./package.json', JSON.stringify(package, null, 2))
       "
     fi
     ;;
+    (add)
+    case $2 in
+      (css)
+      mkdir source/styles
+      touch source/index.css
+      npm i --save-dev postcss-cli postcss-cssnext postcss-import clean-css
+      node -e "
+      const fs = require('fs')
+      const package = JSON.parse(fs.readFileSync('./package.json'))
+      package.scripts['build:development:css'] = 'postcss -u postcss-import -u postcss-cssnext -o build/development/bundle.css source/index.css'
+      package.scripts['build:production:css'] = 'postcss -u postcss-import -u postcss-cssnext source/index.css | cleancss --output build/production/bundle.css'
+      fs.writeFileSync('./package.json', JSON.stringify(package, null, 2))
+
+      fs.writeFileSync(
+        './source/index.html',
+        fs.readFileSync('./source/index.html', 'utf-8')
+          .split('\n')
+          .reduceRight((acc, curr) => {
+            if (/\/head/.test(curr)) {
+              return [
+                ...acc,
+                curr,
+                '  <link rel=\"stylesheet\" type=\"text/css\" href=\"bundle.css\">'
+              ]
+            } else {
+              return [ ...acc, curr ]
+            }
+          }, [])
+          .reverse()
+          .join('\n')
+      )
+      "
+      ;;
+    esac
   esac
 }
