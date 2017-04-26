2b375d27aebaf128d6b5bf4027ea9022940bfc3d
rogerbf <roger@bergfalk.nu>
2017-04-26 13:22:49 +0200

    fix scripts

--- node-project/prettier.sh
+++ node-project/prettier.sh
@@ -8,8 +8,8 @@ prettier () {
   node -e "
   const fs = require('fs')
   const package = JSON.parse(fs.readFileSync('./package.json'))
-  package.scripts['prettier'] = 'prettier-eslint --write'
-  package.scripts['prettier:all'] = 'prettier-eslint --write \"source/**/*.js\"'
+  package.scripts['prettier'] = 'prettier-eslint --prettier.single-quote false --write'
+  package.scripts['prettier:all'] = 'prettier-eslint --prettier.single-quote false --write \"source/**/*.js\"'
   fs.writeFileSync('./package.json', JSON.stringify(package, null, 2))
   "
 }
